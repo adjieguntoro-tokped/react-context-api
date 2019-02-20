d436e088878cbc9ae9b2aba9e0ef52b5420ecc4a
Adjie Guntoro <adjie.guntoro@tokopedia.com>
2019-02-20 23:09:26 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import locales from './locales';
 import './App.css';
+
+// create context component
+const MyContext = React.createContext();
+
+// create provider class
+class MyProvider extends Component {
+  state = {
+    name: "Adjie Guntoro",
+    age: 23,
+    lang: 'id',
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        addMyAge: () => { this.setState(prevState => ({ age: prevState.age + 5 })) },
+        resetMyAge: () => { this.setState({ age: 23 }) },
+        changeLang: lang => () => { this.setState({ lang }) },
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+const Family = (props) => (
+  <div>
+    <Person />
+  </div>
+);
+class Person extends Component {
+  render() {
+    return (
+      <MyContext.Consumer>
+       {({ state: { name, age, lang }, addMyAge, changeLang, resetMyAge }) => (
+          <div>
+            <div>
+              {locales[lang].hello} {name}, {locales[lang]['your age']} {age} {locales[lang]['years old']}.
+            </div>
+            <button onClick={addMyAge}>{locales[lang]['add age']}</button>
+            <button onClick={resetMyAge}>Reset</button>
+            <button onClick={changeLang(lang === 'id' ? 'en' : 'id')}>{locales[lang]['change lang']}</button>
+          </div>
+         )}
+      </MyContext.Consumer>
+      
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyProvider>
+          <header className="App-header">
+            <Family />
+          </header>
+        </MyProvider>
       </div>
     );
   }
---
+++ src/locales.js
@@ -0,0 +1,25 @@
+const english = {
+  hello: 'Hello',
+  'your age': 'your age is',
+  'years old': 'years old',
+  'add age': 'Add age +5',
+  'change lang': 'ganti ke bahasa indonesia'  
+}
+
+
+const indonesian = {
+  hello: 'Halo',
+  'your age': 'usia kamu adalah',
+  'years old': 'tahun',
+  'add age': 'tambah usia +5',
+  'change lang': 'change to english'
+}
+
+
+const dictionary = {
+  en: english,
+  id: indonesian
+}
+
+export default dictionary;
+
